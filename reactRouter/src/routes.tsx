c619eeaae7721cbb998092cd6b9a2c8687a4a9cb
LissandraFischer <145466916+LissandraFischer@users.noreply.github.com>
2023-10-03 13:56:40 +0000

    rotas

---
+++ reactRouter/src/routes.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Home } from "./Pages/Home/Index"
+import { Sobre } from "./Pages/Sobre/Index"
+import {Contato } from "./Pages/Contato/Index"
+
+const router = createBrowserRouter ([
+    {
+        path:"/",
+        element:<Home/>
+    },
+    {
+        path:"/sobre",
+        element:<Sobre/>
+    },
+    {
+        path:"/contato",
+        element:<Contato/>
+    }
+
+])
+export{router};
